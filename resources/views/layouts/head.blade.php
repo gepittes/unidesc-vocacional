effb47b9b1dc5f154df4e49e2f13747f26212f95
gabriel-roque <gabrielrms.dll@gmail.com>
2019-02-20 12:17:52 -0300

    components HEAD and SCRIPTS

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8" />
+<link rel="apple-touch-icon" sizes="76x76" href="{{url('templeteAssets/assets/img/apple-icon.png')}}">
+<link rel="icon" type="image/png" href="{{url('templeteAssets/assets/img/favicon.png')}}">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<title>
+    {{$title}}
+</title>
+<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+<!--     Fonts and icons     -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+<!-- CSS Files -->
+<link href="{{url('templeteAssets/assets/css/bootstrap.min.css')}}" rel="stylesheet" />
+<link href="{{url('templeteAssets/assets/css/now-ui-kit.css?v=1.2.0')}}" rel="stylesheet" />
+<!-- CSS Just for demo purpose, don't include it in your project -->
+<link href="{{url('templeteAssets/assets/demo/demo.css')}}" rel="stylesheet" />
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,22 +2,8 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="{{url('templeteAssets/assets/img/apple-icon.png')}}">
-    <link rel="icon" type="image/png" href="{{url('templeteAssets/assets/img/favicon.png')}}">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>
-        {{$title}}
-    </title>
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-    <!--     Fonts and icons     -->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
-    <!-- CSS Files -->
-    <link href="{{url('templeteAssets/assets/css/bootstrap.min.css')}}" rel="stylesheet" />
-    <link href="{{url('templeteAssets/assets/css/now-ui-kit.css?v=1.2.0')}}" rel="stylesheet" />
-    <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link href="{{url('templeteAssets/assets/demo/demo.css')}}" rel="stylesheet" />
+    @component('layouts.head', ['title' => $title])
+    @endcomponent
 </head>
 
 <body class="login-page sidebar-collapse">
@@ -149,20 +135,8 @@
         </footer>
     </div>
 </div>
-<!--   Core JS Files   -->
-<script src="{{url('templeteAssets/assets/js/core/jquery.min.js')}}" type="text/javascript"></script>
-<script src="{{url('templeteAssets/assets/js/core/popper.min.js')}}" type="text/javascript"></script>
-<script src="{{url('templeteAssets/assets/js/core/bootstrap.min.js')}}" type="text/javascript"></script>
-<!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
-<script src="{{url('templeteAssets/assets/js/plugins/bootstrap-switch.js')}}"></script>
-<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-<script src="{{url('templeteAssets/assets/js/plugins/nouislider.min.js')}}" type="text/javascript"></script>
-<!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
-<script src="{{'templeteAssets/assets/js/plugins/nouislider.min.js'}}" type="text/javascript"></script>
-<!--  Google Maps Plugin    -->
-{{--<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>--}}
-<!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
-<script src="{{url('templeteAssets/assets/js/now-ui-kit.js?v=1.2.0')}}" type="text/javascript"></script>
+    @component('layouts.scripts')
+    @endcomponent
 </body>
 
 </html>
