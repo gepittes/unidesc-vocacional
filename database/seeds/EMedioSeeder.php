9e4bbe5fb09bdce55d4f7f33b1fe8bfe08752c80
gabriel-roque <gabrielrms.dll@gmail.com>
2019-02-27 19:23:19 -0300

    update seeder

--- database/seeds/EMedioSeeder.php
+++ database/seeds/EMedioSeeder.php
@@ -13,10 +13,10 @@ class EMedioSeeder extends Seeder
     public function run()
     {
         DB::table('e_medio')->insert([
-            ['id' => 1, 'serie' => '6', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
-            ['id' => 2, 'serie' => '7', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
-            ['id' => 3, 'serie' => '8', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
-            ['id' => 4, 'serie' => '9', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+            ['id' => 1, 'serie' => '1', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+            ['id' => 2, 'serie' => '2', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+            ['id' => 3, 'serie' => '3', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+            ['id' => 4, 'serie' => 'concluido', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
         ]);
     }
 }
