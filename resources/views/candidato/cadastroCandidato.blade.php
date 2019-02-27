6b5b12b8bd7b637f92491293751ae85f688b6e95
gabriel-roque <gabrielrms.dll@gmail.com>
2019-02-27 14:03:18 -0300

    recuperando dados form na session

--- resources/views/candidato/cadastroCandidato.blade.php
+++ resources/views/candidato/cadastroCandidato.blade.php
@@ -32,21 +32,21 @@
                         <div class="form-row">
                             <div class="form-group col">
                                 <label for="nome">Nome Completo</label>
-                                <input type="text" class="form-control" id="nome" name="nome" placeholder="Insira seu nome">
+                                <input type="text" class="form-control" id="nome" name="nome" placeholder="Insira seu nome" value="{{old('nome')}}">
                                 @if($errors->has('nome'))
                                     <span class="badge badge-danger space-error-bg">{{ $errors->first('nome') }}</span>
                                 @endif
                             </div>
                             <div class="form-group col">
                                 <label for="telefone">Telefone Celular</label>
-                                <input type="number" class="form-control" id="telefone" name="telefone" placeholder="(DD) 00000-0000">{{--todo auto formatar campo--}}
+                                <input type="number" class="form-control" id="telefone" name="telefone" placeholder="(DD) 00000-0000" value="{{old('telefone')}}">{{--todo auto formatar campo--}}
                                 @if($errors->has('telefone'))
                                     <span class="badge badge-danger space-error-bg">{{ $errors->first('telefone') }}</span>
                                 @endif
                             </div>
                             <div class="form-group col">
                                 <label for="email">Email</label>
-                                <input type="email" class="form-control" id="email" name="email" placeholder="nome@gmail.com">
+                                <input type="email" class="form-control" id="email" name="email" placeholder="nome@gmail.com" value="{{old('email')}}">
                                 @if($errors->has('email'))
                                     <span class="badge badge-danger space-error-bg">{{ $errors->first('email') }}</span>
                                 @endif
@@ -56,10 +56,19 @@
                             <div class="form-group col">
                                 <label for="cidade">Cidade</label>
                                 <select class="form-control" id="cidade" name="cidade">
-                                    <option value="" selected>Selecionar</option>
+                                    @if(old('cidade'))
+                                    @else
+                                        <option value="" selected>Selecionar</option>
+                                    @endif
+
                                     @foreach($cidades as $cidade)
-                                        <option value="{{$cidade->nome}}">{{$cidade->nome}}</option>
+                                        @if($cidade->nome == old('cidade'))
+                                            <option value="{{old('cidade')}}" selected>{{old('cidade')}}</option>
+                                        @else
+                                            <option value="{{$cidade->nome}}">{{$cidade->nome}}</option>
+                                        @endif
                                     @endforeach
+
                                 </select>
                                 @if($errors->has('cidade'))
                                     <span class="badge badge-danger space-error-bg">{{ $errors->first('cidade') }}</span>
