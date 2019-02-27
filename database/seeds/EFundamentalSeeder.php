fefc974d555409186877e2334a13ddf7df9d5e40
gabriel-roque <gabrielrms.dll@gmail.com>
2019-02-27 19:24:47 -0300

    Carregando Series Ensino Fundamental

---
+++ database/seeds/EFundamentalSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EFundamentalSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('e_fundamental')->insert([
+            ['id' => 1, 'serie' => '6', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+            ['id' => 2, 'serie' => '7', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+            ['id' => 3, 'serie' => '8', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+            ['id' => 4, 'serie' => '9', 'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s")],
+        ]);
+    }
+}
