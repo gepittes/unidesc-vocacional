ca0a98fd5bccd720eaa0ffddc337db4eb56adbcd
gabriel-roque <gabrielrms.dll@gmail.com>
2019-02-27 13:02:54 -0300

    Exbibindo exibindo validacao View

--- resources/views/candidato/cadastroCandidato.blade.php
+++ resources/views/candidato/cadastroCandidato.blade.php
@@ -33,25 +33,37 @@
                             <div class="form-group col">
                                 <label for="nome">Nome Completo</label>
                                 <input type="text" class="form-control" id="nome" name="nome" placeholder="Insira seu nome">
+                                @if($errors->has('nome'))
+                                    <span class="badge badge-danger space-error-bg">{{ $errors->first('nome') }}</span>
+                                @endif
                             </div>
                             <div class="form-group col">
                                 <label for="telefone">Telefone Celular</label>
                                 <input type="number" class="form-control" id="telefone" name="telefone" placeholder="(DD) 00000-0000">{{--todo auto formatar campo--}}
+                                @if($errors->has('telefone'))
+                                    <span class="badge badge-danger space-error-bg">{{ $errors->first('telefone') }}</span>
+                                @endif
                             </div>
                             <div class="form-group col">
                                 <label for="email">Email</label>
                                 <input type="email" class="form-control" id="email" name="email" placeholder="nome@gmail.com">
+                                @if($errors->has('email'))
+                                    <span class="badge badge-danger space-error-bg">{{ $errors->first('email') }}</span>
+                                @endif
                             </div>
                         </div>
                         <div class="form-row">
                             <div class="form-group col">
                                 <label for="cidade">Cidade</label>
                                 <select class="form-control" id="cidade" name="cidade">
-                                    <option selected>Selecionar</option>
+                                    <option value="" selected>Selecionar</option>
                                     @foreach($cidades as $cidade)
                                         <option value="{{$cidade->nome}}">{{$cidade->nome}}</option>
                                     @endforeach
                                 </select>
+                                @if($errors->has('cidade'))
+                                    <span class="badge badge-danger space-error-bg">{{ $errors->first('cidade') }}</span>
+                                @endif
                             </div>
                             <div class="form-group col">
                                 <label for="serie">Serie</label>
@@ -70,6 +82,9 @@
                                             <option value="concluido">Concluído</option>
                                         </optgroup>
                                 </select>
+                                @if($errors->has('serie'))
+                                    <span class="badge badge-danger space-error-bg">{{ $errors->first('serie') }}</span>
+                                @endif
                             </div>
                             <div class="form-group mt-3">
                                 <button type="submit" class="btn btn-primary">Enviar</button>
