3c2dfc22c86767250c7cf9f6f651050df7e73422
gabriel-roque <gabrielrms.dll@gmail.com>
2019-02-27 13:02:37 -0300

    Validacao de Form Candidato

--- app/Http/Controllers/CandidatoController.php
+++ app/Http/Controllers/CandidatoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Cidade;
+use App\Http\Requests\CandidatoFormRequest;
 use App\Models\Candidato;
 use Illuminate\Http\Request;
 
@@ -37,7 +38,7 @@ class CandidatoController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CandidatoFormRequest $request)
     {
         //ARMAZENA CADASTRO
         $candidato = new Candidato();
