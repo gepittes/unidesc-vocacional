e1b696f099e44354779dd21a0bc66cd8c5332934
gabriel-roque <gabrielrms.dll@gmail.com>
2019-03-16 14:11:51 -0300

    [UPDATE] Ajuste de Identacao

--- app/Models/ResultadoCand.php
+++ app/Models/ResultadoCand.php
@@ -7,7 +7,8 @@ use Illuminate\Support\Facades\DB;
 
 class ResultadoCand extends Model
 {
-    public function resultadoCursos($letraGrupo){
+    public function resultadoCursos($letraGrupo)
+    {
         // RESULTADO DOS CURSOS QUE SE ADEQUAM COM A LETRA DO GRUPO
         $resultadoCursosCand = DB::table('cursos')
             ->select('curso_descricao')
