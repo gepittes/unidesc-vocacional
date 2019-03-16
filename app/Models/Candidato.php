d535e861b1b8f1880bbf2661e92078e2b8652481
gabriel-roque <gabrielrms.dll@gmail.com>
2019-03-16 14:14:39 -0300

    [ADD] Funcao de Insert dos Dados do Candidato MODEL

--- app/Models/Candidato.php
+++ app/Models/Candidato.php
@@ -4,9 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class Candidato extends Model
 {
     Use SoftDeletes;
     protected $dates =['deleted_at'];
+
+    public function storeCand($dadosCandDB)
+    {
+        //INSERT NO BANCO DO REGISTRO DO CANDIDATO
+        DB::table('candidatos')->insert([
+            'nome' => $dadosCandDB->nome,
+            'telefone' => $dadosCandDB->telefone,
+            'email' => $dadosCandDB->email,
+            'cidade' => $dadosCandDB->cidade,
+            'serie' => $dadosCandDB->serie,
+            'resultado_curso' => $dadosCandDB->resultado_curso,
+            'visitor' => $dadosCandDB->visitor,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+    }
 }
