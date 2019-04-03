ccae93d734945540f61a8fee3224a17d6b115edb
gabriel-roque <gabrielrms.dll@gmail.com>
2019-04-03 10:32:04 -0300

    [REFECTOR] Refatorando retorn das funcoes das Models

--- app/Models/CursoDescricao.php
+++ app/Models/CursoDescricao.php
@@ -9,13 +9,9 @@ class CursoDescricao extends Model
 {
     public function descCand($cat)
     {
-        $caracteristicaCand = DB::table('curso_descricaos')
+        return DB::table('curso_descricaos')
             ->select('caracteristica', 'descricao')
             ->where('grupo_letra', '=', "$cat")->get();
 
-
-//        dd($caracteristicaCand);
-
-        return $caracteristicaCand;
     }
 }
--- app/Models/ResultadoCand.php
+++ app/Models/ResultadoCand.php
@@ -10,10 +10,8 @@ class ResultadoCand extends Model
     public function resultadoCursos($letraGrupo)
     {
         // RESULTADO DOS CURSOS QUE SE ADEQUAM COM A LETRA DO GRUPO
-        $resultadoCursosCand = DB::table('cursos')
+        return DB::table('cursos')
             ->select('curso_descricao')
             ->where('grupo_curso', '=', "$letraGrupo")->get();
-
-        return $resultadoCursosCand;
     }
 }
