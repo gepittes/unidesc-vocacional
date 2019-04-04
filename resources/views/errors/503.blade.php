c13146216df504145ddaef634d1bf807feebed99
gabriel-roque <gabrielrms.dll@gmail.com>
2019-04-04 07:26:50 -0300

    [ADD|UPDATE] Paginas de 404 e 503 Finalizadas

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="{{asset('templeteAssets/assets/img/favicon.png')}}">
 
-    <title>Teste Vocacional | 404</title>
+    <title>Teste Vocacional | Manutenção</title>
 
     <!-- Google font -->
     <link href="{{url('assets404/fonts/font-oswald.css')}}" rel="stylesheet">
@@ -37,11 +37,11 @@
 
     <div class="notfound">
         <div class="notfound-404">
-            <h1>404</h1>
+            <h1>503</h1>
         </div>
-        <h2>Página não Encontrada</h2>
-        <p>Você pode esta tentando acessar uma pagina que não existe em nosso sistema. Entre em contato conosco caso seja necessário.</p>
-        <a href="{{route('home')}}">Início</a>
+        <h2>Página em Manutenção</h2>
+        <p>Pedimos desculpas por não atender no momento.<br>Estamos trabalhando o mais rápido possível.</p>
+        <a href="{{route('home')}}">VOLTE MAIS TARDE!</a>
         <div class="notfound-social">
             <a href="https://www.facebook.com/unidesc/" target="_blank"><i class="fa fa-facebook"></i></a>
         </div>
