e6d79f7a36774b58446b68406de8cd5c9a9a3d6a
Jadilson Guedes <jadilson12@gmail.com>
2019-05-19 17:35:30 -0300

    [UPDATE]: model e migrate Questao

---
+++ app/Models/Questao/Questao.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Questao;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Questao extends Model
+{
+    // Renomear a tabela corretamente
+    protected $table = 'questoes';
+
+}
---
+++ database/migrations/2019_05_18_202539_create_questaos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestaosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questoes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string( 'altenativa');
+            $table->string( 'nome');
+            $table->integer( 'grupo');
+            $table->integer('opcao');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists( 'questoes');
+    }
+}
