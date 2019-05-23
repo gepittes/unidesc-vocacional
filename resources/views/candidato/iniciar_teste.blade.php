e43a203113e8bc86c848170a47d43616c0ce650e
gabriel-roque <gabrielrms.dll@gmail.com>
2019-05-23 01:08:12 -0300

    [ADD] Adicionando Modal de Avisos ao candidato

--- resources/views/candidato/iniciar_teste.blade.php
+++ resources/views/candidato/iniciar_teste.blade.php
@@ -4,6 +4,44 @@
     <div class="wrapper">
         <div class="container">
 
+            {{-- MODAL(Mensagem de Orientacao)--}}
+
+            <div class="modal fade mt-5" tabindex="-1" role="dialog" id="avisoCand">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title font-weight-bold">Orientações</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <p class="text-justify font-weight-normal"><strong>1-</strong> Você deve ler as alternativas
+                                do campo <strong>A</strong> e depois do campo <strong>B</strong> de cada grupo e
+                                escolher
+                                uma alternativa que seja do seu interesse.
+                                <span class="font-italic text-danger">
+                                    Caso não tenha interesse por nenhuma das alternativas pode deixar em branco.
+                                </span>
+                            </p>
+                            <p class="text-justify font-weight-normal"><strong>2-</strong> De acordo com a pontuação de
+                                cada grupo, na última página, você irá
+                                identificar as áreas de conhecimento que você apresenta mais
+                                habilidades e interesse.</p>
+                            <p class="text-justify font-weight-normal"><strong>3-</strong> Com base no seu resultado,
+                                pesquise sobre as profissões relacionadas a
+                                cada área (o que faz, onde atua, mercado de trabalho e etc)
+                                e veja aquela com a qual tem maior afinidade para estudo.</p>
+                        </div>
+                        <div class="modal-footer">
+                            <span></span>
+                            <button type="button" class="btn btn-success" data-dismiss="modal"><strong>OK!</strong>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             {{--   AQUI VEM O WIZARD  --}}
 
             <form action="{{route('recebe.questoes.cand')}}" method="post">
@@ -170,71 +208,73 @@
 
         $(document).ready(function () {
 
-                {{-- Escreve os enunciados das questoes nos labels --}}
+            $('#avisoCand').modal('show');
+
+
+            {{-- Escreve os enunciados das questoes nos labels --}}
                 $questoes = $.getJSON('/api/questoes', function (data) {
 
-                    {{-- Ponto de partida do grupo A no JSON --}}
+                {{-- Ponto de partida do grupo A no JSON --}}
                     $j = 0;
-                    for($i = 1; $i <= 12; $i++){
+                for ($i = 1; $i <= 12; $i++) {
 
-                        document.getElementById("GpAA"+$i).innerHTML = "<strong class='text-info'>A</strong> <i class=\"fas fa-angle-right\"></i> " +
-                            $questoes.responseJSON[$j].texto_alternativa;
+                    document.getElementById("GpAA" + $i).innerHTML = "<strong class='text-info'>A</strong> <i class=\"fas fa-angle-right\"></i> " +
+                        $questoes.responseJSON[$j].texto_alternativa;
 
-                        document.getElementById("GpAB"+$i).innerHTML = "<strong class='text-danger'>B</strong> <i class=\"fas fa-angle-right\"></i> " +
-                            $questoes.responseJSON[$j+1].texto_alternativa;
+                    document.getElementById("GpAB" + $i).innerHTML = "<strong class='text-danger'>B</strong> <i class=\"fas fa-angle-right\"></i> " +
+                        $questoes.responseJSON[$j + 1].texto_alternativa;
 
-                        $j+=2;
-                    }
+                    $j += 2;
+                }
 
-                    {{-- Ponto de partida do grupo B no JSON --}}
+                {{-- Ponto de partida do grupo B no JSON --}}
                     $j = 25;
-                    for($i = 1; $i <= 12; $i++){
+                for ($i = 1; $i <= 12; $i++) {
 
-                        document.getElementById("GpBA"+$i).innerHTML = "<strong class='text-info'>A</strong> <i class=\"fas fa-angle-right\"></i> " +
-                            $questoes.responseJSON[$j].texto_alternativa;
-                        document.getElementById("GpBB"+$i).innerHTML = "<strong class='text-danger'>B</strong> <i class=\"fas fa-angle-right\"></i> " +
-                            $questoes.responseJSON[$j+1].texto_alternativa;
+                    document.getElementById("GpBA" + $i).innerHTML = "<strong class='text-info'>A</strong> <i class=\"fas fa-angle-right\"></i> " +
+                        $questoes.responseJSON[$j].texto_alternativa;
+                    document.getElementById("GpBB" + $i).innerHTML = "<strong class='text-danger'>B</strong> <i class=\"fas fa-angle-right\"></i> " +
+                        $questoes.responseJSON[$j + 1].texto_alternativa;
 
-                        $j+=2;
-                    }
+                    $j += 2;
+                }
 
-                    {{-- Ponto de partida do grupo C no JSON --}}
+                {{-- Ponto de partida do grupo C no JSON --}}
                     $j = 48;
-                    for($i = 1; $i <= 12; $i++){
+                for ($i = 1; $i <= 12; $i++) {
 
-                        document.getElementById("GpCA"+$i).innerHTML = "<strong class='text-info'>A</strong> <i class=\"fas fa-angle-right\"></i> " +
-                            $questoes.responseJSON[$j].texto_alternativa;
-                        document.getElementById("GpCB"+$i).innerHTML = "<strong class='text-danger'>B</strong> <i class=\"fas fa-angle-right\"></i> " +
-                            $questoes.responseJSON[$j+1].texto_alternativa;
+                    document.getElementById("GpCA" + $i).innerHTML = "<strong class='text-info'>A</strong> <i class=\"fas fa-angle-right\"></i> " +
+                        $questoes.responseJSON[$j].texto_alternativa;
+                    document.getElementById("GpCB" + $i).innerHTML = "<strong class='text-danger'>B</strong> <i class=\"fas fa-angle-right\"></i> " +
+                        $questoes.responseJSON[$j + 1].texto_alternativa;
 
-                        $j+=2;
-                    }
+                    $j += 2;
+                }
 
-                    {{-- Ponto de partida do grupo D no JSON --}}
+                {{-- Ponto de partida do grupo D no JSON --}}
                     $j = 72;
-                    for($i = 1; $i <= 12; $i++){
+                for ($i = 1; $i <= 12; $i++) {
 
-                        document.getElementById("GpDA"+$i).innerHTML = "<strong class='text-info'>A</strong> <i class=\"fas fa-angle-right\"></i> " +
-                            $questoes.responseJSON[$j].texto_alternativa;
-                        document.getElementById("GpDB"+$i).innerHTML = "<strong class='text-danger'>B</strong> <i class=\"fas fa-angle-right\"></i> " +
-                            $questoes.responseJSON[$j+1].texto_alternativa;
+                    document.getElementById("GpDA" + $i).innerHTML = "<strong class='text-info'>A</strong> <i class=\"fas fa-angle-right\"></i> " +
+                        $questoes.responseJSON[$j].texto_alternativa;
+                    document.getElementById("GpDB" + $i).innerHTML = "<strong class='text-danger'>B</strong> <i class=\"fas fa-angle-right\"></i> " +
+                        $questoes.responseJSON[$j + 1].texto_alternativa;
 
-                        $j+=2;
-                    }
+                    $j += 2;
+                }
 
-                    {{-- Ponto de partida do grupo E no JSON --}}
+                {{-- Ponto de partida do grupo E no JSON --}}
                     $j = 96;
-                    for($i = 1; $i <= 12; $i++){
+                for ($i = 1; $i <= 12; $i++) {
 
-                        document.getElementById("GpEA"+$i).innerHTML = "<strong class='text-info'>A</strong> <i class=\"fas fa-angle-right\"></i> " +
-                            $questoes.responseJSON[$j].texto_alternativa;
-                        document.getElementById("GpEB"+$i).innerHTML = "<strong class='text-danger'>B</strong> <i class=\"fas fa-angle-right\"></i> " +
-                            $questoes.responseJSON[$j+1].texto_alternativa;
+                    document.getElementById("GpEA" + $i).innerHTML = "<strong class='text-info'>A</strong> <i class=\"fas fa-angle-right\"></i> " +
+                        $questoes.responseJSON[$j].texto_alternativa;
+                    document.getElementById("GpEB" + $i).innerHTML = "<strong class='text-danger'>B</strong> <i class=\"fas fa-angle-right\"></i> " +
+                        $questoes.responseJSON[$j + 1].texto_alternativa;
 
-                        $j+=2;
-                    }
-                });
-
+                    $j += 2;
+                }
+            });
 
 
             $('#smartwizard').smartWizard({
