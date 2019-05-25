61381891907631dc6c5b6d3a797a6ec720d9e56f
Jadilson Guedes <jadilson12@gmail.com>
2019-05-24 23:31:50 -0300

    [ADD]  Adicionou o EmailServices para Candidato

---
+++ app/Services/EmailServices.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Mail\MailCandidato;
+use Illuminate\Support\Facades\Mail;
+
+class EmailServices{
+
+    // Enviar email para o cardidato
+    public static function sendEmail($data)
+    {
+        Mail::to($data->email)->send(new MailCandidato($data));
+        return redirect()->back();
+    }
+
+}
