3bf7cc7e5f3eb8b26222919ee0c02439832c7b58
gabriel-roque <gabrielrms.dll@gmail.com>
2019-06-28 09:05:24 -0300

    [MOVE] Reorganizando Diretorios

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,27 +1,32 @@
 <head>
+    {{-- META TAGS --}}
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-    <title>
-        {{$title}}
-    </title>
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-    {{-- FONTS --}}
-    <link rel="stylesheet" href="{{url('/fonts/fonts.css')}}">
-    {{-- ICONS Font Awesonme --}}
-    <link rel="stylesheet" href="{{url('/icons/css/all.min.css')}}">
-    {{-- CSS Files Custom--}}
-    <link rel="stylesheet" href="{{url('css/responsive.css')}}">
-    {{-- CSS Files Template--}}
-    <link href="{{url('/assets/template/css/bootstrap.min.css')}}" rel="stylesheet" />
-    <link href="{{url('/assets/template/css/now-ui-kit.css?v=1.2.0')}}" rel="stylesheet" />
-    {{--Jquery--}}
-    <script src="{{url('/js/jquery.min.js')}}"></script>
+    <meta name="description" content="Teste Vocacional da UNIDESC">
+    <meta name="keywords" content="Teste Vocacional, Unidesc, Sistemas, TI, Engenharia de Software, PHP, Laravel">
+    <meta name="author" content="Gabriel Roque & Jadilson Guedes">
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport'/>
     {{-- Cor da aba - MOBILE --}}
     <meta name="theme-color" content="#0e7494">
     <meta name="apple-mobile-web-app-status-bar-style" content="#0e7494">
+    {{-- META TAGS --}}
+
+    <title>{{$title}}</title>
+
+    {{-- FONTS --}}
+    <link rel="stylesheet" href="{{asset('/assets/fonts/fonts.css')}}">
+    {{-- ICONS Font Awesonme --}}
+    <link rel="stylesheet" href="{{asset('/assets/icons/css/all.min.css')}}">
+    {{-- CSS Files Custom--}}
+    <link rel="stylesheet" href="{{asset('/assets/css/responsive.css')}}">
+    {{-- CSS Files Template--}}
+    <link href="{{asset('/assets/template/css/bootstrap.min.css')}}" rel="stylesheet" />
+    <link href="{{asset('/assets/template/css/now-ui-kit.css?v=1.2.0')}}" rel="stylesheet" />
+    {{--Jquery--}}
+    <script src="{{asset('/assets/js/jquery.min.js')}}"></script>
     {{--Wizard--}}
-    <link href="{{url('/assets/wizard/css/smart_wizard.css')}}" rel="stylesheet" />
-    <link href="{{url('/assets/wizard/css/smart_wizard_theme_dots.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('/assets/wizard/css/smart_wizard.css')}}" rel="stylesheet" />
+    <link href="{{asset('/assets/wizard/css/smart_wizard_theme_dots.css')}}" rel="stylesheet" type="text/css" />
     {{-- Google Charts --}}
-    <script type="text/javascript" src="{{url('/js/loader.js')}}"></script>
+    <script type="text/javascript" src="{{asset('/assets/js/loader.js')}}"></script>
 </head>
--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,19 +1,19 @@
 {{-- Core JS Files --}}
-<script src="{{url('/assets/template/js/core/jquery.min.js')}}" type="text/javascript"></script>
-<script src="{{url('/assets/template/js/core/popper.min.js')}}" type="text/javascript"></script>
-<script src="{{url('/assets/template/js/core/bootstrap.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('/assets/template/js/core/jquery.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('/assets/template/js/core/popper.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('/assets/template/js/core/bootstrap.min.js')}}" type="text/javascript"></script>
 {{-- Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ --}}
-<script src="{{url('/assets/template/js/plugins/bootstrap-switch.js')}}"></script>
+<script src="{{asset('/assets/template/js/plugins/bootstrap-switch.js')}}"></script>
 {{-- Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ --}}
-<script src="{{url('/assets/template/js/plugins/nouislider.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('/assets/template/js/plugins/nouislider.min.js')}}" type="text/javascript"></script>
 {{-- Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker --}}
-<script src="{{url('/assets/template/js/plugins/nouislider.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('/assets/template/js/plugins/nouislider.min.js')}}" type="text/javascript"></script>
 {{-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc --}}
-<script src="{{url('/assets/template/js/now-ui-kit.js?v=1.2.0')}}" type="text/javascript"></script>
+<script src="{{asset('/assets/template/js/now-ui-kit.js?v=1.2.0')}}" type="text/javascript"></script>
 {{-- Font Awesome --}}
-<script src="{{url('/icons/js/all.min.js')}}"></script>
+<script src="{{asset('/assets/icons/js/all.min.js')}}"></script>
 {{--Jquery Mask--}}
-<script src="{{url('/js/jquery.mask.js')}}"></script>
+<script src="{{asset('/assets/js/jquery.mask.js')}}"></script>
 {{--Wizard--}}
-<script src="{{url('/assets/wizard/js/jquery.smartWizard.js')}}"></script>
+<script src="{{asset('/assets/wizard/js/jquery.smartWizard.js')}}"></script>
 
